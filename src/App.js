1a69b9bdc98de5d0bd7fc66f516740e94d710ed0
JimmyMcBride <mcbride967@gmail.com>
2020-06-26 15:44:03 -0500

    init comit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Editor from "for-editor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [value, setValue] = useState("");
+  function handleChange(value) {
+    setValue(value);
+  }
+  console.log(value);
+  return <Editor language="en" value={value} onChange={handleChange} />;
 }
-
-export default App;
